9cb6e23b37e14541de9ab9e09945961d9ba4c01a
Simon Andersen <anders_s@epita.fr>
2018-07-04 16:48:10 +0200

    [Fix] Fixed username in database.scala

--- src/main/scala/com/cassandra/DataBase.scala
+++ src/main/scala/com/cassandra/DataBase.scala
@@ -109,11 +109,9 @@ object CassandraDB {
       sc.stop()
 
       if (res != null) {
-        // ToDo: convert updatedOn to java.Instant
         User(Id[User](res.getString("id")),
              Instant.parse(res.getString("updatedOn")),
              URI.create(res.getString("image")),
-             res.getString("id"),
              res.getBool("deleted"))
       }
       else {
