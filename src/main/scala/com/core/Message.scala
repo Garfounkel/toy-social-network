ba47ac2e330180df758cf776d187e0b07d603cc0
Karim Daoud <karim.daoud@epita.fr>
2018-07-04 15:15:53 +0200

    [Message] Adding the message class

---
+++ src/main/scala/com/core/Message.scala
@@ -0,0 +1,28 @@
+package com.core
+
+import java.time.Instant
+import com.kafka._
+import com.utils._
+
+import org.json4s._
+import org.json4s.native.Serialization
+import org.json4s.native.Serialization.{read, write}
+import java.time.format.DateTimeFormatter
+import com.github.kardapoltsev.json4s.javatime.{InstantSerializer}
+import org.apache.kafka.common.serialization.{Deserializer, Serializer}
+import java.util
+import java.nio.charset._
+
+
+case class Message(id: Id[Post],
+                updatedOn: Instant,
+                from: Id[User],
+                to: Id[User],
+                text: String,
+                deleted: Boolean)
+
+object Message {
+  implicit val topic: Topic[Message] = new Topic[Message] {
+    val value = "messages"
+  }
+}
